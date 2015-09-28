4d615ceb48cec8f3f1729599f51ed4820300035e
CatsFromMars <mduque@andrew.cmu.edu>
2015-09-27 21:06:33 -0400

    added weatherbox

--- Assets/Scripts/WeatherSync/SkylightWeather.cs
+++ Assets/Scripts/WeatherSync/SkylightWeather.cs
@@ -8,11 +8,15 @@ public class SkylightWeather : MonoBehaviour {
 	//Wheather Effects
 	private ParticleSystem currentWeatherEffect;
 	public ParticleSystem rain;
+	public ParticleSystem clouds;
+	public ParticleSystem snow;
 
 	private int rainMin = 200;
 	private int rainMax = 531;
 	private int clearMin = 800;
-	private int clearMax = 802;
+	private int clearMax = 801;
+	private int cloudMin = 802;
+	private int cloudMax = 804;
 	private int snowMin = 600;
 	private int snowMax = 622;
 
@@ -26,12 +30,19 @@ public class SkylightWeather : MonoBehaviour {
 
 		//Rain
 		if (w.conditionID >= rainMin && w.conditionID <= rainMax) {
-			//if(currentWeatherEffect != null) currentWeatherEffect.Stop();
+			if(currentWeatherEffect != null) currentWeatherEffect.Stop();
 			rain.Play();
 			currentWeatherEffect = rain;
 		}
+		//Clouds
+		else if(w.conditionID >= cloudMin && w.conditionID <= cloudMax) {
+			if(currentWeatherEffect != null) currentWeatherEffect.Stop();
+			clouds.Play();
+			currentWeatherEffect = clouds;
+		}
 		else {
 			if(currentWeatherEffect != null) currentWeatherEffect.Stop ();
+			currentWeatherEffect = null;
 		}
 	}
 }
